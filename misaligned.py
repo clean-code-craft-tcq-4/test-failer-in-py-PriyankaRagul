e1255a3d8afeac3f6d762d33c07e1b63ffac93c1
PriyankaRagul <105204385+PriyankaRagul@users.noreply.github.com>
2022-06-23 22:41:00 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -8,7 +8,6 @@ def print_color_map():
             # returning last pairnumber to check if it's 25
     return len(major_colors) * len(minor_colors),i * 5 + j
 
-
 result,last_pair_number = print_color_map()
 assert(result == 25)
 # including testcase to check if its following 1 based index
