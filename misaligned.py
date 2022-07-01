d148d9a3132880dbe5f14abedddc1326b0c5c495
PriyankaRagul <105204385+PriyankaRagul@users.noreply.github.com>
2022-07-01 23:37:41 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,17 +1,37 @@
-global last_pair_number;
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+pair_number_end = len(major_colors) * len(minor_colors)
+pair_number_start = 1
+
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    color_map = []
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            lengthOfColors=len(minor_colors)
-            pairNumber=i * lengthOfColors + j+1
-            print(f'{pairNumber:<3} | {major:<7} | {minor}')
-            # returning last pairnumber to check if it's 25
-    return len(major_colors) * len(minor_colors),pairNumber
+            pair_number = get_pairnumber(i,j)
+            [first_sep_idx,second_sep_idx] = get_separator_index(format_string(pair_number, major, minor))
+            #print(format_string(pair_number, major, minor))
+            color_map.append((pair_number, [first_sep_idx,second_sep_idx]))
+    return len(major_colors) * len(minor_colors),color_map
 
-result,last_pair_number = print_color_map()
+def format_string(pair_number, major_color, minor_color):
+  return f'{pair_number:3} | {major_color:6} | {minor_color}' #added proper space to align separator
+
+def get_separator_index(formatted_string):
+    [first_sep_idx,second_sep_idx] = [index for index, separator in enumerate(formatted_string) if separator == '|']
+    return [first_sep_idx,second_sep_idx]
+
+def get_pairnumber(major_color, minor_color):
+    return major_color * len(major_colors) + minor_color + 1
+
+#Test function to check if separator is properly aligned and pair number is within range
+def test_color_map(color_map):
+    assert(color_map[0][0]==1) # check if the first pair number is 1
+    for i in range(pair_number_start,pair_number_end):
+        assert([color_map[i][1][0],color_map[i][1][1]]==[color_map[pair_number_end-i][1][0],color_map[pair_number_end-i][1][1]])
+        assert(color_map[i][0] == i+1)
+
+result,color_map = print_color_map()
+print(color_map)
 assert(result == 25)
-# including testcase to check if its following 1 based index
-assert(last_pair_number == 25)
-print("All is well (maybe!)\n")
+test_color_map(color_map)
+print("All is well :)\n")
